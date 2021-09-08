2d3ba7941a3df6ad6938a8cef70b76b57a40edf8
Username <jwaffensmith@gmail.com>
2021-09-08 15:32:54 -0700

    migrated models

--- main_app/models.py
+++ main_app/models.py
@@ -1,39 +1,37 @@
-import time
-
 from django.db import models
 from django.db.models import Model, CharField, TextField, DateTimeField, ForeignKey, ImageField
 from django.db.models.deletion import CASCADE
 from django.db.models.fields import DateField, IntegerField, DecimalField
 from django.db.models.fields.files import ImageField
+from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Hike(Model):
     name = CharField(max_length=50)
-    img = ImageField(max_length=600, default="https://images.pexels.com/photos/4448861/pexels-photo-4448861.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
-    description = TextField(max_length=1000)
+    img_one = ImageField(max_length=500, default="https://images.pexels.com/photos/4448861/pexels-photo-4448861.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
+    img_two = ImageField(max_length=500, blank= True)
+    img_three = ImageField(max_length=500, blank= True)
+    description = TextField(max_length=1000, blank= True)
     location = CharField(max_length=200)
-    length = DecimalField(decimal_places=2, max_digits=7, default=0)
-    elevation_gain = IntegerField(default=0)
+    length = DecimalField(decimal_places=2, max_digits=7, default=0, blank= True)
+    elevation_gain = IntegerField(default=0, blank= True)
     hike_rating = IntegerField(default=1)
-    hike_date = DateField(auto_now=False)
+    hike_date = DateField(auto_now=False, blank= True)
     created_at = DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=CASCADE)
 
     def __str__(self):
         return self.name
     
-    def get_length(self):
-        return time.strftime("%-M:%S", time.gmtime(self.length))
-
     class Meta: 
-        ordering = ["created_at"]
-
+        ordering = ["-created_at"]
 
 
 class Comment(Model):
     hike = ForeignKey(Hike, on_delete=CASCADE, related_name="comments")
-    content = CharField(max_length=200)
+    content = TextField(max_length=500)
     created_at = DateTimeField(auto_now_add=True)
+    user = ForeignKey(User, on_delete=CASCADE, related_name="comments")
 
     def __str__(self):
         return f"{self.pk} - {self.hike.title}"
@@ -41,7 +39,10 @@ class Comment(Model):
 
 
 class Profile(Model):
-    username = CharField(max_length=30)
-    avatar = CharField(max_length=200, default="https://source.unsplash.com/twukN12EN7c")
-    location = CharField
+    user = models.OneToOneField(User, on_delete=CASCADE, related_name="profile")
+    avatar = CharField(max_length=400, default="https://source.unsplash.com/twukN12EN7c")
+    location = CharField(max_length=100)
+
+    def __str__(self):
+        return self.user.username
 
