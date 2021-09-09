f27416db5b4a9969c08fde8ad2a1c41a07610919
Username <jwaffensmith@gmail.com>
2021-09-09 13:41:47 -0700

    fixed login and signup form issue

---
+++ main_app/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.db.models import fields
+from .models import Profile 
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(required=True, label='First Name')
+    last_name = forms.CharField(required=True, label='Last Name')
+
+    class Meta: 
+        model = User
+        fields = ("first_name", "last_name", "username", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.email = self.cleaned_data["last_name"]
+        if commit:
+            user.save()
+        return user
+
+
+class ProfileForm(forms.ModelForm):
+        class Meta:
+            model = Profile
+            fields = ('location', 'avatar')
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
+from .forms import SignupForm, ProfileForm
+from .models import User 
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -13,17 +14,30 @@ class About(TemplateView):
 class Profile(TemplateView):
     template_name = "profile.html"
 
-class Signup(View):
-    def get(self, request):
-        form = UserCreationForm()
-        context = {"form": form}
-        return render(request, "registration/signup.html", context)
+class Signup(TemplateView):
+    
+    template_name = "signup.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['signup_form'] = SignupForm()
+        context['profile_form'] = ProfileForm()
+        return context
+
     def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
+        signup_form = SignupForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if signup_form.is_valid() and profile_form.is_valid():
+            user = signup_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
             login(request, user)
-            return redirect("profile")
+            return redirect("/profile/")
         else: 
-            context = {"form": form}
+            context = {"signup_form": signup_form, "profile_form": profile_form}
             return render(request, "registration/signup.html", context)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
