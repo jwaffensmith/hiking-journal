060a0dcf66e5560665c16978c6a2f2702f1324e9
Username <jwaffensmith@gmail.com>
2021-09-13 15:51:37 -0700

    user can reset password with reset email, user can change password

--- hike_project/settings.py
+++ hike_project/settings.py
@@ -33,6 +33,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
