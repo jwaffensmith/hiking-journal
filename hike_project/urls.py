ad07788b17ffd00157252e9621b02e5bf711e5ae
Username <jwaffensmith@gmail.com>
2021-09-13 12:44:40 -0700

    added error pages

--- hike_project/urls.py
+++ hike_project/urls.py
@@ -6,3 +6,8 @@ urlpatterns = [
     path('', include('main_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+handler404 = 'main_app.views.custom_page_not_found_view'
+handler500 = 'main_app.views.custom_error_view'
+handler403 = 'main_app.views.custom_permission_denied_view'
+handler400 = 'main_app.views.custom_bad_request_view'
--- main_app/views.py
+++ main_app/views.py
@@ -178,3 +178,15 @@ class SortView(TemplateView):
         hikes = hike_filter.qs
         context = {'hikes': hikes, 'hike_filter': hike_filter}
         return render(request, "sort_hikes.html", context)
+
+def custom_page_not_found_view(request, exception):
+    return render(request, "errors/404.html", {})
+
+def custom_error_view(request, exception=None):
+    return render(request, "errors/500.html", {})
+
+def custom_permission_denied_view(request, exception=None):
+    return render(request, "errors/403.html", {})
+
+def custom_bad_request_view(request, exception=None):
+    return render(request, "errors/400.html", {})
